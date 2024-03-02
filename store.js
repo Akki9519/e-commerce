95d33fde45b969f466ff0987226d1fbaafe9a213
Akash Kesharwani <akashkesharwani81@gmail.com>
2024-03-02 08:50:26 +0530

    akki

---
+++ store.js
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import CartReducer from './redux/CardReducer'
+export default configureStore({
+    reducer:{
+        cart:CartReducer
+    }
+})
